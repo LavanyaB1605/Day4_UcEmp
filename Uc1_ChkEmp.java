83e0650d2d2634a23d4420c2bfc5f10aad102431
LavanyaB1605 <lavanya.bhaskaran16@gmail.com>
2022-10-05 23:00:18 +0530

    Added files

---
+++ Uc1_ChkEmp.java
@@ -0,0 +1,11 @@
+// Attendance check for employee
+public class Uc1_ChkEmp {
+    public static void main(String[] args) {
+        double empCheck = Math.random();
+        if(empCheck > 0.5)
+            System.out.println("Employee is present");
+            else
+            System.out.println("Employee is absent");
+
+    }
+}
